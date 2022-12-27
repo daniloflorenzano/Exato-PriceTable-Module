dafe7ac83b55881d7ee4cf7aedd3c2f5bf091606
Danilo Florenzano <danilo.maia@aedb.br>
2022-12-27 19:11:00 -0300

    Item.cs: define os valores dos descontos e quando serao aplicados na criacao do item

--- Domain/Entities/Item.cs
+++ Domain/Entities/Item.cs
@@ -7,19 +7,41 @@ namespace Domain
         public readonly Guid ExternalId = Guid.NewGuid();
 
         public string Description { get; set; }
-        public ItemType Type { get; private set; }
-        public double Price { get; set; }
-        public int InitialAmount { get; set; }
-        public int LimitAmount { get; set; }
+        public double InitalPrice { get; set; }
+        public List<double>? PriceSequence { get; set; }
+        public List<int>? AmountLimitsToApplyDiscount { get; set; }
+        public readonly ItemType Type;
 
 
-        public Item(string description, double price, int initialAmount, int limitAmount)
+        /// <summary>
+        /// Construtor de Item para ser usado em tabela de preço fixo.
+        /// </summary>
+        /// <param name="description">Descrição/Nome do item</param>
+        /// <param name="initialPrice">Preço inicial</param>
+        public Item(string description, double initialPrice)
         {
             Description = description;
+            InitalPrice = initialPrice;
             Type = 0;
-            Price = price;
-            InitialAmount = initialAmount;
-            LimitAmount = limitAmount;
+        }
+
+        /// <summary>
+        /// Construtor de Item para ser usado em tabelas de desconto cumulativo e não cumulativo.
+        /// </summary>
+        /// <param name="description">Descrição/Nome do item</param>
+        /// <param name="initialPrice">Preço inicial</param>
+        /// <param name="priceSequence">Sequência de preços que serão aplicados conforme os descontos</param>
+        /// <param name="amountLimitsToApplyDiscount">Lista dos limites de quantidade para aplicação dos descontos</param>
+        /// <exception cref="ArgumentException"></exception>
+        public Item(string description, double initialPrice, List<double> priceSequence, List<int> amountLimitsToApplyDiscount)
+        {
+            if (priceSequence.Count != amountLimitsToApplyDiscount.Count - 1)
+                throw new ArgumentException("priceSequence list must have one more element than changePriceLimits list");
+            
+            Description = description;
+            InitalPrice = initialPrice;
+            PriceSequence = priceSequence;
+            AmountLimitsToApplyDiscount = amountLimitsToApplyDiscount;
         }
     }
 }
