ae8b634d2a2d67ec9356bc3ab42d57f8f9ed2b8e
Danilo Florenzano <danilo.maia@aedb.br>
2023-01-21 18:31:30 -0300

    ItemHandlerTests.cs: testa metodo SegregateItems

---
+++ UnitTests/ItemHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using Application.Handlers;
+using Domain.Abstractions;
+using Domain.Entities;
+using FakeItEasy;
+using FluentAssertions;
+
+namespace UnitTests;
+
+public class ItemHandlerTests
+{
+    private readonly IRepositoryFactory _repositoryFactory;
+    private readonly Guid _tableExternalGuid;
+
+    public ItemHandlerTests()
+    {
+        _repositoryFactory = A.Fake<IRepositoryFactory>();
+        _tableExternalGuid = new Guid();
+    }
+
+    [Test]
+    public void SegregateItems_Should_Return_Three_Lists()
+    {
+        // Arrange
+        ItemHandler itemHandler = new(_repositoryFactory, _tableExternalGuid);
+        var listOfItems = new List<Item>
+        {
+            new Item("A", 10.0m),
+            new Item("A", 10.0m),
+            new Item("B", 10.0m),
+            new Item("C", 10.0m),
+            new Item("A", 10.0m),
+            new Item("B", 10.0m),
+            new Item("A", 10.0m),
+            new Item("C", 10.0m),
+        };
+        
+        // Act
+        var groupsOfItems = itemHandler.SegregateItems(listOfItems);
+
+        // Assert
+        groupsOfItems.Count.Should().Be(3);
+    }
+}
