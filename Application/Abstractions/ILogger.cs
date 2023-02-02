744b1864df7f68150b78872ebcc0429ba88f9077
Danilo Florenzano <danilo.maia@aedb.br>
2023-02-01 23:40:25 -0300

    Adiciona metodos de warn e error ao log

--- Application/Abstractions/ILogger.cs
+++ Application/Abstractions/ILogger.cs
@@ -3,4 +3,7 @@
 public interface ILogger
 {
     public void Information(string message);
+    public void Warning(string message);
+    public void Error(string message);
+
 }
--- Application/Wrappers/SerilogWrapper.cs
+++ Application/Wrappers/SerilogWrapper.cs
@@ -9,6 +9,12 @@ public sealed class SerilogWrapper : ILogger
     public void Information(string message) => 
         Initialization().Information("{Message}", message);
 
+    public void Warning(string message) =>
+        Initialization().Warning("{Message}", message);
+
+    public void Error(string message) =>
+        Initialization().Error("{Message}", message);
+    
     private Logger Initialization()
     {
         using var log = new LoggerConfiguration()
