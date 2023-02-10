b09812ee51adcef764deb0ff0afc9e1b80b4fc80
Danilo Florenzano <danilo.maia@aedb.br>
2023-02-10 20:09:55 -0300

    mapeia a entidade Table no dbContext

--- Infraestructure/ApplicationDbContext.cs
+++ Infraestructure/ApplicationDbContext.cs
@@ -1,13 +1,33 @@
-﻿using Domain;
-using Domain.Entities;
+﻿using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infraestructure;
 
 public class ApplicationDbContext : DbContext
 {
+    public DbSet<Table> Tables { get; set; }
+    
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Table>(
+                eb =>
+                {
+                    eb.HasNoKey();
+                    eb.ToView("registered_tables");
+                    eb.Property(v => v.Id).HasColumnName("id");
+                    eb.Property(v => v.ExternalId).HasColumnName("external_id");
+                    eb.Property(v => v.Name).HasColumnName("name");
+                    eb.Property(v => v.Description).HasColumnName("description");
+                    eb.Property(v => v.Type).HasColumnName("type");
+                    eb.Property(v => v.Active).HasColumnName("active");
+                    eb.Property(v => v.ExpirationDate).HasColumnName("expiration_date");
+                    eb.Property(v => v.CreationDate).HasColumnName("creation_date");
+                });
     }
 }
